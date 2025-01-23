e8a552cbe8c61a50fba13dcb2cb2ecfdf2079d22
leap <Bunleab.sok@student.cadt.edu.kh>
2025-01-23 14:00:33 +0700

    airport(class)

---
+++ Airport.java
@@ -0,0 +1,10 @@
+public class Airport {
+    private int id;
+    private String Name;
+    private String Location;
+
+    public Airport(int id, String Location) {
+        this.id = id;
+        this.Location = Location;
+    }
+}
