8af8d4ed5fe640e08a9aba4b04e70cc82e3a8434
leap <Bunleab.sok@student.cadt.edu.kh>
2025-01-23 14:01:27 +0700

    Main

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        Airport airport = new Airport(1, "New York");
+        Flight flight = new Flight();
+        System.out.println();
+        System.out.println("Flight Details:");
+        System.out.println("ID: " + flight.getId());
+        // System.out.println("Origin: " + flight.getOrigin());
+        // System.out.println("Destination: " + flight.getDestination());
+        // System.out.println("Departure Date: " + flight.getDepartureDate());
+        // System.out.println("Economy Seats: " + flight.getEconomyCapacity());
+        // System.out.println("Business Seats: " + flight.getBusinessCapacity());
+        
+    }
+}
