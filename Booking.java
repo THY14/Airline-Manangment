e260255a1c52196b1f6407c617347939b5e3caa6
sothearasaroeun <sotheara.saroeun.911@gmail.com>
2025-01-29 23:50:18 +0700

    Second Draft

--- Booking.java
+++ Booking.java
@@ -6,13 +6,27 @@ class Booking {
     private String bookingDate;
     private String status;
     private double totalPrice;
+    private String paymentMethod;
 
-    public Booking(String bookingId, String passengerId, String flightNumber, String seatNumber, String bookingDate, String status, double totalPrice) {
+    // For Return Ticket
+    private boolean isRoundTrip;
+    private String returnFlightNumber;
+    private String returnDate;
+    private String returnSeatNumber;
+
+    public Booking(String bookingId, String passengerId, String flightNumber, String seatNumber, String bookingDate, String status, double totalPrice, String paymentMethod, boolean isRoundTrip, String returnFlightNumber, String returnDate, String returnSeatNumber) {
         this.bookingId = bookingId;
         this.passengerId = passengerId;
         this.flightNumber = flightNumber;
         this.seatNumber = seatNumber;
         this.bookingDate = bookingDate;
+        this.status = status;
+        this.totalPrice = totalPrice;
+        this.paymentMethod = paymentMethod;
+        this.isRoundTrip = isRoundTrip;
+        this.returnFlightNumber = returnFlightNumber;
+        this.seatNumber = returnSeatNumber;
+        this.bookingDate = returnDate;
         this.status = status;
         this.totalPrice = totalPrice;
     }
--- Employee.java
+++ Employee.java
@@ -1,50 +1,28 @@
 public class Employee {
-    private String id;
+    private String employeeID;
     private String firstName;
     private String lastName;
+    private String Gender;
+    private String DoB;
     private String email;
-    private String Tel;
-    private double salary;
+    private String phoneNumber;
     private String position;
     private String department;
+    private double salary;
     private String hireDate;
 
-    public Employee(String employeeId, String firstN, String lastN, String posit, String depart, String mail, String number, double fee, String hire) {
-        id = employeeId;
-        firstName = firstN;
-        lastName = lastN;
-        position = posit;
-        department = depart;
-        email = mail;
-        Tel = number;
-        salary = fee;
-        hireDate = hire;
+    public Employee (String employeeID, String firstName, String lastName, String Gender, String DoB, String email, String phoneNumber, String position, String department, double salary, String hireDate) {
+        this.employeeID = employeeID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.Gender = Gender;
+        this.DoB = DoB;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.position = position;
+        this.department = department;
+        this.salary = salary;
+        this.hireDate = hireDate;
     }
-
-    public String getId(){ return id; }
-    
-    public void setFirstName(String firstN){ firstName = firstN; }
-    public String getFirstName(){ return firstName; }
-
-    public void setLastName(String lastN){ lastName = lastN;}
-    public String getLastName(){ return lastName; }
-
-    public void setPosition(String posit){ position = posit;  }
-    public String getPosition(){ return position; }
-
-    public void setDepartment(String depart){ depart = department; }
-    public String getDepartment(){ return department;}
-
-    public void setEmail(String mail){ mail = email; }
-    public String getEmail(){ return email; }
-
-    public void setTel(String number){ number = Tel; }
-    public String getTel(){ return Tel; }
-
-    public void setSalary(double fee){ fee = salary;}
-    public double getSalary(){ return salary;}
-
-    public void setHireDate(String hire){ hire = hireDate; }
-    public String getHireDate(){ return hireDate;}   
 
 }
