681574297c74fc20b8f1e802252135c429a2d8d5
leap <Bunleab.sok@student.cadt.edu.kh>
2025-02-11 13:54:24 +0700

    test

--- Airport.java
+++ Airport.java
@@ -1,10 +1,87 @@
 public class Airport {
-    private int id;
-    private String Name;
-    private String Location;
+    private String code ;
+    private String name;
+    private String location;
+    private int openyear;
+    private String country;
+    private static long annualFlights;
+    private double totalPassengerTraffic;
+    private boolean international;
 
-    public Airport(int id, String Location) {
-        this.id = id;
-        this.Location = Location;
+    public Airport(String code, String name, String location, int openyear, String country, double totalPassengerTraffic, boolean international) {
+        annualFlights++;
+        this.code = code;
+        this.name = name;
+        this.location = location;
+        this.openyear = openyear;
+        this.country = country;
+        this.totalPassengerTraffic = totalPassengerTraffic;
+        this.international = international;
+    }
+
+
+    
+
+    public String getCode() {
+        return code;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getLocation() {
+        return location;
+    }
+
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+
+    public int getOpenyear() {
+        return openyear;
+    }
+    
+
+    public void setOpenyear(int openyear) {
+        this.openyear = openyear;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    public static long getAnnualFlights() {
+        return annualFlights;
+    }
+    public double getAveragePassengerTraffic() {
+        return totalPassengerTraffic / annualFlights;
+    }
+    public void setTotalPassengerTraffic(double totalPassengerTraffic, double passengerIncrease) {
+        this.totalPassengerTraffic = totalPassengerTraffic +  passengerIncrease;
+    }
+
+    public double getTotalPassengerTraffic() {
+        return totalPassengerTraffic;
+    }
+    
+    public boolean isInternational() {
+        return international;
+    }
+    public void setInternational(boolean international) {
+        this.international = international;
     }
 }
--- Flight.java
+++ Flight.java
@@ -1,29 +1,147 @@
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 public class Flight {
-    private int id;
+    private static  int id;
 	private Airplane airplane;
 	private Airport origin;
 	private Airport destination;
-	private LocalDateTime departureTime;
-	private LocalDateTime arrivalTime;
+	private LocalDateTime departureTime; 
+	private LocalDateTime arrivalTime; 
 	private boolean delayed;
 	private int bookedEconomy;
 	private int bookedBusiness;
-	private Employee[] Employees;
-	private Passenger[] passengers;
-	private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd::HH:mm:ss");
-
-
-    public Flight(){
-        delayed = false;
-        bookedEconomy = 0;
-        bookedBusiness = 0;
-        Employees = new Employee[10];
+	private ArrayList <Employee> employees;
+	private ArrayList <Passenger> passengers;
+	public Flight(Airplane airplane, Airport origin, Airport destination, LocalDateTime departureTime,
+			LocalDateTime arrivalTime, boolean delayed, int bookedEconomy, int bookedBusiness, ArrayList <Employee> employees,
+			ArrayList <Passenger> passengers) {
+		this.airplane = airplane;
+		this.origin = origin;
+		this.destination = destination;
+		this.departureTime = departureTime;
+		this.arrivalTime = arrivalTime;
+		this.delayed = delayed;
+		this.bookedEconomy = bookedEconomy;
+		this.bookedBusiness = bookedBusiness;
+		this.employees = new ArrayList<>();
+		this.passengers = new ArrayList<>();
+	}
+	public static int getId() {
+        return id;
     }
 
+    public static void setId(int id) {
+        Flight.id = id;
+    }
 
+    public Airplane getAirplane() {
+        return airplane;
+    }
+
+    public void setAirplane(Airplane airplane) {
+        this.airplane = airplane;
+    }
+
+    public Airport getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(Airport origin) {
+        this.origin = origin;
+    }
+
+    public Airport getDestination() {
+        return destination;
+    }
+
+    public void setDestination(Airport destination) {
+        this.destination = destination;
+    }
+
+    public LocalDateTime getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(LocalDateTime departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public LocalDateTime getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(LocalDateTime arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public boolean isDelayed() {
+        return delayed;
+    }
+
+    public void setDelayed(boolean delayed) {
+        this.delayed = delayed;
+    }
+
+    public int getBookedEconomy() {
+        return bookedEconomy;
+    }
+
+    public void setBookedEconomy(int bookedEconomy) {
+        this.bookedEconomy = bookedEconomy;
+    }
+
+    public int getBookedBusiness() {
+        return bookedBusiness;
+    }
+
+    public void setBookedBusiness(int bookedBusiness) {
+        this.bookedBusiness = bookedBusiness;
+    }
+    public int getTotalPassengers() {
+        return bookedEconomy + bookedBusiness;
+    }
+
+    public String checkFlightStatus() {
+        return delayed ? "Flight is delayed" : "Flight is on time";
+    }
+
+    public ArrayList<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(ArrayList<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public ArrayList<Passenger> getPassengers() {
+        return passengers;
+    }
+
+    public void setPassengers(ArrayList<Passenger> passengers) {
+        this.passengers = passengers;
+    }
     
-
+    // Methods to add employees and passengers
+    public void addEmployee(Employee employee) { this.employees.add(employee); }
+    public void addPassenger(Passenger passenger) { this.passengers.add(passenger); }
+    
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Flight{");
+        sb.append("airplane=").append(airplane);
+        sb.append(", origin=").append(origin);
+        sb.append(", destination=").append(destination);
+        sb.append(", departureTime=").append(departureTime);
+        sb.append(", arrivalTime=").append(arrivalTime);
+        sb.append(", delayed=").append(delayed);
+        sb.append(", bookedEconomy=").append(bookedEconomy);
+        sb.append(", bookedBusiness=").append(bookedBusiness);
+        sb.append(", employees=").append(employees);
+        sb.append(", passengers=").append(passengers);
+        sb.append('}');
+        return sb.toString();
+    }
 }
+
