8897f348846a04ce1881d6e5256e1b94c49672e6
methy <methymuy@gmail.com>
2025-02-13 08:38:27 +0700

    2

--- Airplane.java
+++ Airplane.java
@@ -1,40 +1,152 @@
+import java.util.ArrayList;
+
 public class Airplane {
     private int id;
-	private int EconomyCapacity;
-	private int BusinessCapacity;
-	private String model;
+    private int economyCapacity;
+    private int businessCapacity;
+    private String model;
+    private ArrayList<Double> flightHours;  
+    private static String airline;  
+    private static double fuelCapacity;  
+    private String password; 
 
-
-    public Airplane(int id, int economyCapacity, int businessCapacity, String model) {
+    public Airplane(int id, int economyCapacity, int businessCapacity, String model,
+                    ArrayList<Double> flightHours, String airline, double fuelCapacity, String password) {
         this.id = id;
-        this.EconomyCapacity = economyCapacity;
-        this.BusinessCapacity = businessCapacity;
+        this.economyCapacity = economyCapacity;
+        this.businessCapacity = businessCapacity;
         this.model = model;
-    }
-    public int getId() {
-        return id;
-    }
-    public int getEconomyCapacity() {
-        return EconomyCapacity;
-    }
-    public int getBusinessCapacity() {
-        return BusinessCapacity;
-    }
-    public String getModel() {
-        return model;
-    }
-    public void setModel(String model) {
-        this.model = model;
-    }
-    public void setEconomyCapacity(int EconomyCapacity) {
-        this.EconomyCapacity = EconomyCapacity;
-    }
-    public void setBusinessCapacity(int businessCapacity) {
-        this.BusinessCapacity = businessCapacity;
+        this.flightHours = flightHours;
+        this.airline = airline;
+        this.fuelCapacity = fuelCapacity;
+        this.password = password;  
     }
 
-    public void setId(int id) {
-        this.id = id;
+    private boolean authenticate(String password) {
+        return this.password.equals(password);
     }
 
+  
+    public static String getAirline(String password) {
+        if (authenticateStatic(password)) {
+            return airline;
+        } else {
+            return "Access Denied";
+        }
+    }
+
+    public static double getFuelCapacity(String password) {
+        if (authenticateStatic(password)) {
+            return fuelCapacity;
+        } else {
+            return -1;  
+        }
+    }
+
+    public ArrayList<Double> getFlightHours(String password) {
+        if (authenticate(password)) {
+            return flightHours;
+        } else {
+            return null;  
+        }
+    }
+
+    public double getFlightHour(int index, String password) {
+        if (authenticate(password)) {
+            if (index >= 0 && index < flightHours.size()) {
+                return flightHours.get(index);
+            }
+        }
+        return -1;  
+    }
+
+    // Method to add a flight hour to the flight hours list, requires password authentication
+    public void addFlightHour(double hour, String password) {
+        if (authenticate(password)) {
+            flightHours.add(hour);
+        }
+    }
+
+    // Getter and Setter for other attributes (no authentication required for non-sensitive data)
+    public int getId(String password) {
+        if (authenticate(password)) {
+            return id;
+        } else {
+            return -1;  // Return -1 to indicate access denial
+        }
+    }
+
+    public void setId(int id, String password) {
+        if (authenticate(password)) {
+            this.id = id;
+        }
+    }
+
+    public int getEconomyCapacity(String password) {
+        if (authenticate(password)) {
+            return economyCapacity;
+        } else {
+            return -1;  // Return -1 to indicate access denial
+        }
+    }
+
+    public void setEconomyCapacity(int economyCapacity, String password) {
+        if (authenticate(password)) {
+            this.economyCapacity = economyCapacity;
+        }
+    }
+
+    public int getBusinessCapacity(String password) {
+        if (authenticate(password)) {
+            return businessCapacity;
+        } else {
+            return -1;  // Return -1 to indicate access denial
+        }
+    }
+
+    public void setBusinessCapacity(int businessCapacity, String password) {
+        if (authenticate(password)) {
+            this.businessCapacity = businessCapacity;
+        }
+    }
+
+    public String getModel(String password) {
+        if (authenticate(password)) {
+            return model;
+        } else {
+            return "Access Denied";
+        }
+    }
+
+    public void setModel(String model, String password) {
+        if (authenticate(password)) {
+            this.model = model;
+        }
+    }
+
+    // Setter for flight hours list (direct access without password control)
+    public void setFlightHours(ArrayList<Double> flightHours, String password) {
+        if (authenticate(password)) {
+            this.flightHours = flightHours;
+        }
+    }
+
+    // Static methods to set the airline and fuel capacity (requires password)
+    public static void setAirline(String airline, String password) {
+        if (authenticateStatic(password)) {
+            Airplane.airline = airline;
+        }
+    }
+
+    public static void setFuelCapacity(double fuelCapacity, String password) {
+        if (authenticateStatic(password)) {
+            Airplane.fuelCapacity = fuelCapacity;
+        }
+    }
+
+
+    private static boolean authenticateStatic(String password) {
+        
+        return password.equals("globalPassword");
+    }
 }
--- Passenger.java
+++ Passenger.java
@@ -1,64 +1,133 @@
+import java.util.ArrayList;
+
 public class Passenger {
     private int id;
-	private String firstName;
-	private String lastName;
-    private String passportNumber;
-	private String Tel;
-	private String email;
+    private String firstName;
+    private String lastName;
+    private ArrayList<String> passportNumbers;
+    private ArrayList<String> Tel;
+    private ArrayList<String> email;
     private String Gender;
-    public Passenger(int id, String firstName, String lastName, String Tel, String email,String passportNumber,String Gender) {
+    private static int totalPassengers = 0;
+
+    // Password field for access control
+    private String password;
+
+    // Constructor
+    public Passenger(int id, String firstName, String lastName, 
+                     ArrayList<String> Tel, ArrayList<String> email, 
+                     ArrayList<String> passportNumbers, String Gender, String password) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.Tel = Tel;
         this.email = email;
-        this.passportNumber = passportNumber;
-        this.Gender = Gender;	
-    }
-
-    public int getId() {
-        return id;
-    }
-    public String getFirstName() {
-        return firstName;
-    }
-    public String getLastName() {
-        return lastName;
-    }
-    public String getTel() {
-        return Tel;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public String getGender() {
-        return Gender;
-    }
-    public String getpassportNumber () {
-        return passportNumber;
-    }
-    public void setid(int id) {
-        this.id = id;
-    }
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-    public void setTel(String Tel) {
-        this.Tel = Tel;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setGender(String Gender) {
+        this.passportNumbers = passportNumbers;
         this.Gender = Gender;
+        this.password = password;  // Set the password during creation
+        totalPassengers++;    
     }
 
-    public void setPassportNumber(String passportNumber) {
-        this.passportNumber = passportNumber;
+    // Method to verify password before accessing private fields
+    public boolean authenticate(String password) {
+        return this.password.equals(password);
     }
 
+    // Getter methods (after authentication)
+    public String getFirstName(String password) {
+        if (authenticate(password)) {
+            return firstName;
+        } else {
+            return "Access Denied";
+        }
+    }
+
+    public String getLastName(String password) {
+        if (authenticate(password)) {
+            return lastName;
+        } else {
+            return "Access Denied";
+        }
+    }
+
+    public ArrayList<String> getTel(String password) {
+        if (authenticate(password)) {
+            return Tel;
+        } else {
+            return null;  
+        }
+    }
+
+    public ArrayList<String> getEmail(String password) {
+        if (authenticate(password)) {
+            return email;
+        } else {
+            return null;
+        }
+    }
+
+    public String getGender(String password) {
+        if (authenticate(password)) {
+            return Gender;
+        } else {
+            return null;
+        }
+    }
+
+    public ArrayList<String> getPassportNumbers(String password) {
+        if (authenticate(password)) {
+            return passportNumbers;
+        } else {
+            return null;
+        }
+    }
+
+    // Setters with authentication (password check before setting values)
+    public void setFirstName(String password, String firstName) {
+        if (authenticate(password)) {
+            this.firstName = firstName;
+        } else {
+            System.out.println("Access Denied: Incorrect Password");
+        }
+    }
+
+    public void setLastName(String password, String lastName) {
+        if (authenticate(password)) {
+            this.lastName = lastName;
+        } else {
+            System.out.println("Access Denied: Incorrect Password");
+        }
+    }
+
+    public void setTel(String password, ArrayList<String> Tel) {
+        if (authenticate(password)) {
+            this.Tel = Tel;
+        } else {
+            System.out.println("Access Denied: Incorrect Password");
+        }
+    }
+
+    public void setEmail(String password, ArrayList<String> email) {
+        if (authenticate(password)) {
+            this.email = email;
+        } else {
+            System.out.println("Access Denied: Incorrect Password");
+        }
+    }
+
+    public void setGender(String password, String Gender) {
+        if (authenticate(password)) {
+            this.Gender = Gender;
+        } else {
+            System.out.println("Access Denied: Incorrect Password");
+        }
+    }
+
+    public void setPassportNumbers(String password, ArrayList<String> passportNumbers) {
+        if (authenticate(password)) {
+            this.passportNumbers = passportNumbers;
+        } else {
+            System.out.println("Access Denied: Incorrect Password");
+        }
+    }
 }
