bd50aaf7caaae65fc04a5e3a427e83039c52b605
leap <Bunleab.sok@student.cadt.edu.kh>
2025-02-27 10:14:20 +0700

    2

--- Flight.java
+++ Flight.java
@@ -1,7 +1,6 @@
-import java.util.ArrayList;
-import USER.Person;
 import USER.Passenger;
-import USER.Employee;
+import USER.Person;
+import java.util.ArrayList;
 public class Flight extends TravelEntity {
     private boolean delayed;
     private int bookedEconomy;
